4d5239e5d223e941958d733acb097befdc89d701
Brandon Arreola-Soto <barreolasoto@gmail.com>
2023-07-11 18:39:22 -0500

    Added choices Finished random function for computer

--- javascript.js
+++ javascript.js
@@ -1,6 +1,22 @@
-//Added function for computer to play against opponent
+/// Adding choices to the program
+const choices = ['Rock', 'Paper', 'Scissors'];
+
+///Adding a way to randomize choices for the computer 
 function getComputerChoice() {
+    const randomNumber = (Math.floor(Math.random() * 3));
+
+    return choices[randomNumber];
+    
+}
+
+console.log(getComputerChoice());
+
+///Creating rounds for computer and player
+function playRound(playerSelection, computerSelection) {
+
+
 
 }
 
-console.log('hello world');
+
+
